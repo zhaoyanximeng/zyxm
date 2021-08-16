3700eaf8aad3cac36c7d544085da75501d9c4cfb
zhaoyanximeng <1368788830@qq.com>
2021-08-16 23:07:40 +0800

    服务间基本调用

--- micro-project/cmd/server/user_server.go
+++ micro-project/cmd/server/user_server.go
@@ -3,15 +3,25 @@ package main
 import (
 	"context"
 	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/client"
+	"log"
+	"micro-project/proto/course"
 	"micro-project/proto/user"
 )
 
 type UserService struct {
+	client client.Client
+}
 
+func NewUserService(client client.Client) *UserService {
+	return &UserService{client: client}
 }
 
 func (u *UserService) Test(ctx context.Context, in *user.UserRequest, out *user.UserResponse) error {
 	out.Ret = "user" + in.Id
+	cs := course.NewCourseService("go.micro.api.zyxm.course", u.client)
+	rsp, _ := cs.ListForTop(ctx, &course.ListRequest{Size: 10})
+	log.Println(rsp.Result)
 	return nil
 }
 
@@ -20,7 +30,9 @@ func main() {
 
 	service.Init()
 
-	err := user.RegisterUserServiceHandler(service.Server(),new(UserService))
+	course.NewCourseService("go.micro.api.zyxm.user", service.Client())
+
+	err := user.RegisterUserServiceHandler(service.Server(), NewUserService(service.Client()))
 	if err != nil {
 		panic(err)
 	}
